7fdbb51f8eb5130a5035e600bc6c2592f019ee4e
ahmedsamyh <ahmedsamyh10@gmail.com>
2024-12-19 07:33:06 +0000

    Update bot.stable.py

--- bot.stable.py
+++ bot.stable.py
@@ -6,33 +6,18 @@ import yt_dlp
 from typing import *
 from dotenv import load_dotenv
 import asyncio
-import coloredlogs, logging
 
-# Init Logging
-logging.basicConfig(level=logging.INFO)
-coloredlogs.install(level=logging.INFO)
-logger: logging.Logger = logging.getLogger("bot")
+import my_logging
 
-FFMPEG_OPTS = {'options': '-vn'}
+my_logging.init()
 
 MIN_HTTP_BODY_LEN=2000
 
-# TODO: Get this file's name dynamically instead of hardcoding.
-SOURCE_CODE_FILENAME="bot.stable.py"
+SOURCE_CODE_FILENAME=f"{os.path.splitext(os.path.basename(__file__))[0]}.stable.py"
 
-# TODO: Use Context.current_parameter, etc for parsing of arguments
 # TODO: Remove song from queue when current song ends; Have to !!stop to remove from queue rn.
 # TODO: Implement something on on_member_join
-# TODO: Look at 'https://github.com/yt-dlp/yt-dlp/wiki/FAQ'
-
-def log_info(msg: str):
-    logger.info(msg)
-
-def log_error(msg: str):
-    logger.error(msg)
-
-def log_debug(msg: str):
-    logger.debug(msg)
+# TODO: Implement command parsing on_message_edit
 
 # Helpers
 intents = ds.Intents.default()
@@ -47,13 +32,30 @@ class MiscCog(cmds.Cog, name="Miscellaneous"):
     def __init__(self, bot):
         self.bot = bot
 
-    @cmds.command("ping", help="Command for testing if the bot is online; bot should reply with 'pong!'")
+    async def cog_command_error(self, ctx: cmds.Context, error: Exception) -> None:
+        assert type(ctx.command) == cmds.Command
+        embed = ds.Embed(title="Error")
+        if isinstance(error, cmds.CommandInvokeError):
+            error = error.original
+        embed.description = f"Error: {error}"
+
+        embed.description += f"\nUsage: {ctx.command.usage}"
+        my_logging.bot_error(f"{self.qualified_name}Cog :: {type(error)}")
+        await ctx.send(embed=embed)
+
+    @cmds.command("swapcase", help="Inverts the case of the input.", usage="swap <text>")
+    async def swapcase(self, ctx: cmds.Context, *, text: str):
+        if ctx.author == bot.user:
+            return
+        await ctx.send(text.swapcase())
+
+    @cmds.command("ping", help="Command for testing if the bot is online; bot should reply with 'pong!'", usage="ping")
     async def ping(self, ctx):
         if ctx.author == bot.user:
             return
         await ctx.channel.send("pong!")
 
-    @cmds.command("src", help="Prints the source code for this bot.")
+    @cmds.command("src", help="Prints the source code for this bot.", usage="src")
     async def src(self, ctx):
         if ctx.author == bot.user:
             return
@@ -61,96 +63,74 @@ class MiscCog(cmds.Cog, name="Miscellaneous"):
         try:
             with open(SOURCE_CODE_FILENAME, 'rb') as f:
                 f.seek(0, os.SEEK_END)
-                filesize: int = f.tell()
-                log_info(f"Length of '{SOURCE_CODE_FILENAME}': {filesize}")
+                filesize = f.tell()
+                my_logging.bot_info(f"Length of '{SOURCE_CODE_FILENAME}': {filesize}")
                 f.seek(0)
-
-                # Send file in chunks as a message
-                # await ctx.send("`", silent=True)
-                # while f.tell() < filesize:
-                #     chunk: str = f.read(MIN_HTTP_BODY_LEN).decode('utf-8')
-                #     log_info(f"Read {len(chunk)} bytes")
-                #     await ctx.send(chunk, silent=True)
-                # await ctx.send("`", silent=True)
-
 
                 # Send the whole file as a file
                 file = ds.File(f, filename=SOURCE_CODE_FILENAME)
                 await ctx.send(file=file)
         except FileNotFoundError:
-            log_error(f"File '{SOURCE_CODE_FILENAME}' doesn't exist!")
-            log_info("Please run build.sh to copy bot.py -> bot.stable.py")
+            my_logging.bot_error(f"File '{SOURCE_CODE_FILENAME}' doesn't exist!")
+            my_logging.bot_info("Please run build.sh to copy bot.py -> bot.stable.py")
             await ctx.send("ERROR: It seems like i was deployed improperly...", silent=True)
 
-
-
-    @cmds.command("poop", help="Command for testing if the bot is online; bot should reply with 'pong!'")
+    @cmds.command("poop", help="Hehe poop.", usage="poop")
     async def poop(self, ctx):
         if ctx.author == bot.user:
             return
         await ctx.reply("Shit yourself nigger")
 
-    @cmds.command("av", help="Displays the given user's avatar.")
-    async def av(self, ctx, *args):
+    @cmds.command("av", help="Displays the given user's avatar.", usage="av <member> [server_avatar = false]", aliases=["avatar"])
+    async def av(self, ctx, member: ds.Member, server_avatar: bool = False):
         if ctx.author == bot.user:
             return
-        if len(args) < 1:
-            await ctx.send("ERROR: Please provide the user to display the avatar!", delete_after=5.0, silent=True)
-            return
 
-        user_id = args[0]
-
-        # renove non-digits
-        user_id = user_id.removeprefix('<')
-        user_id = user_id.removeprefix('@')
-        user_id = user_id.removesuffix('>')
-
-        # convert user id from str -> int
-        user_id = int(user_id)
-
-        log_info(f"fetching {user_id}...")
-
-        user: user = await bot.fetch_user(user_id)
-
-        log_info(f"fetched {user}")
-
-        log_info(f"avatar for {user}: {user.display_avatar}")
-
-        await ctx.send(user.display_avatar)
-
+        if server_avatar:
+            await ctx.send(member.display_avatar)
+        else:
+            await ctx.send(member.avatar)
+    
 class MusicCog(cmds.Cog, name="Music"):
-    def __init__(self, bot):
+    def __init__(self, bot) -> None:
         self.bot = bot
-        yt_dlp_options = {
-            "outtmpl": f"{self.DOWNLOAD_PATH}%(id)s.mp3",
+        yt_dlp_options: yt_dlp.YDLOpts = {
             "format": "bestaudio",
             "cookiefile": "cookies.txt",
             # "extract_audio": True,
             "windowsfilenames": False,
             "overwrites": True,
-            "cachedir": f"{self.DOWNLOAD_PATH}/cache",
             # "default_search": "auto",
             # "no_warnings": True,
             # "quiet": True,
-            "logger": logging.getLogger("yt_dlp")
+            "logger": my_logging.logging.getLogger("yt_dlp")
         }
         self.music_queue : List[dict] = []
         self.ytdlp = yt_dlp.YoutubeDL(yt_dlp_options)
 
+    async def cog_command_error(self, ctx: cmds.Context, error: Exception) -> None:
+        assert type(ctx.command) == cmds.Command
+
+        embed = ds.Embed(title="Error")
+        if isinstance(error, cmds.CommandInvokeError):
+            error = error.original
+        if isinstance(error, yt_dlp.utils.UnsupportedError) or isinstance(error, yt_dlp.utils.DownloadError):
+            embed.description = f"Error: That is not a valid youtube link!"
+        else:
+            embed.description = f"Error: {error}"
+
+        embed.description += f"\nUsage: {ctx.command.usage}"
+        my_logging.bot_error(f"{self.qualified_name}Cog :: {type(error)}")
+        await ctx.send(embed=embed)
+
     async def play_audio(self, ctx, player: ds.VoiceClient, info_dict):
         title: str = info_dict['title']
-        log_info(f"Playing '{title}'...")
+        my_logging.bot_info(f"Playing '{title}'...")
         await ctx.send(f"Playing '{title}'...", silent=True)
 
-        try:
-            # src = f"{self.DOWNLOAD_PATH}{id}.mp3"
-            player.play(FFmpegPCMAudio(info_dict["url"], **FFMPEG_OPTS))
-        except Exception as err:
-            await ctx.send(f"ERROR: Failed to play {title}: {err}", silent=True)
-            log_error(f"{err}")
-            return
+        player.play(FFmpegPCMAudio(info_dict["url"], options="-vn"))
 
-    @cmds.command("queue", help="Lists the songs in the queue.")
+    @cmds.command("queue", help="Lists the songs in the queue.", usage="queue")
     async def queue(self, ctx):
         if ctx.author == bot.user:
             return
@@ -166,25 +146,14 @@ class MusicCog(cmds.Cog, name="Music"):
         await ctx.send(msg, silent=True)
 
     # TODO: Find a way to check if the supplied song is already in the queue WITHOUT querying for the video info because that takes time.
-    @cmds.command("play", help="Play youtube videos")
-    async def play(self, ctx, *args):
+    @cmds.command("play", help="Play youtube videos", usage="play <youtube-link>")
+    async def play(self, ctx, link: str):
         async with ctx.typing():
-            if (len(args) < 1):
-                await ctx.send("ERROR: Please provide the youtube link of the song!", silent=True)
-                return
-
-            link = args[0]
-
             if not ctx.author.voice:
                 await ctx.send("Please join a Voice Channel and run the command!", silent=True)
                 return
 
-            try:
-                info_dict = self.ytdlp.extract_info(link, download=False)
-            except yt_dlp.utils.UnsupportedError:
-                log_error(f"Invalid youtube link '{link}'")
-                await ctx.send(f"ERROR: Invalid youtube link '{link}'", silent=True)
-                return
+            info_dict = self.ytdlp.extract_info(link, download=False)
 
             id = info_dict["id"]
             title = info_dict["title"]
@@ -196,10 +165,7 @@ class MusicCog(cmds.Cog, name="Music"):
                     return
 
             # make the bot join vc
-            if not ctx.guild.voice_client: # error would be thrown if bot already connected, this stops the error
-                player: ds.VoiceClient = await ctx.author.voice.channel.connect()
-            else:
-                player: ds.VoiceClient = ctx.guild.voice_client
+            player: ds.VoiceClient = ctx.guild.voice_client if ctx.guild.voice_client else await ctx.author.voice.channel.connect()
 
             if player.is_playing():
                 assert(len(self.music_queue) > 0)
@@ -211,7 +177,7 @@ class MusicCog(cmds.Cog, name="Music"):
 
                 await self.play_audio(ctx, player, info_dict)
 
-    @cmds.command("stop", help="Stops the currently playing song, if any.")
+    @cmds.command("stop", help="Stops the currently playing song, if any.", usage="stop")
     async def stop(self, ctx):
         if ctx.guild.voice_client:
             ctx.guild.voice_client.stop()
@@ -225,7 +191,7 @@ class MusicCog(cmds.Cog, name="Music"):
             else:
                 await ctx.send("Playing next song in queue...", silent=True)
                 next_song_link = self.music_queue[0]["link"]
-                await play(self, ctx, next_song_link)
+                await self.play(ctx, next_song_link)
         else:
             await ctx.send(f"No song is playing!", silent=True)
 
@@ -255,7 +221,7 @@ class MusicCog(cmds.Cog, name="Music"):
                     ctx.voice_client.stop()
 
             current_song = self.music_queue.pop(0)['info']['title']
-            log_info(f"Stopped playing {current_song}")
+            my_logging.bot_info(f"Stopped playing {current_song}")
 
             if await no_next_song(): return
 
@@ -266,7 +232,7 @@ class MusicCog(cmds.Cog, name="Music"):
 
             await self.play_audio(ctx, player, next_song['info'])
 
-    @cmds.command("pause", help="Paused the currently playing song, if any.")
+    @cmds.command("pause", help="Paused the currently playing song, if any.", usage="pause")
     async def pause(self, ctx):
         if ctx.guild.voice_client:
             assert(len(self.music_queue) > 0)
@@ -279,7 +245,7 @@ class MusicCog(cmds.Cog, name="Music"):
         else:
             await ctx.send(f"No song is currently playing", delete_after=10.0, silent=True)
 
-    @cmds.command("resume", help="Resumes the currently paused song, if any.")
+    @cmds.command("resume", help="Resumes the currently paused song, if any.", usage="resume")
     async def resume(self, ctx):
         if ctx.guild.voice_client:
             # Disconnect from VC instead of asserting
@@ -292,7 +258,6 @@ class MusicCog(cmds.Cog, name="Music"):
                 await ctx.send(f"Resumed {title}...", delete_after=10.0, silent=True)
         else:
             await ctx.send(f"No song is currently paused/playing", delete_after=10.0, silent=True)
-
 
 class TouhouCog(cmds.Cog, name='Touhou'):
     def __init__(self, bot):
@@ -311,24 +276,52 @@ class TouhouCog(cmds.Cog, name='Touhou'):
                 'https://media.tenor.com/v5oS9ZOVq0cAAAAM/marisa-kirisame-touhou.gif',
         ]
 
+    async def cog_command_error(self, ctx: cmds.Context, error: Exception) -> None:
+        assert type(ctx.command) == cmds.Command
 
-    @cmds.command("marisad", help="Marisa. 1% Chance for something special :D")
-    async def marisad(self, ctx):
+        embed = ds.Embed(title="Error")
+        embed.description = ""
+        if isinstance(error, cmds.CommandInvokeError):
+            error = error.original
+
+        embed.description += f"\nUsage: {ctx.command.usage}"
+        my_logging.bot_error(f"{self.qualified_name}Cog :: {type(error)}")
+        await ctx.send(embed=embed)
+
+
+    @cmds.command("marisad", help="Marisa. 1% Chance for something special :D", usage="marisad")
+    async def marisad(self, ctx: cmds.Context) -> None:
         # TODO: Make it so we dynamically search "marisad" on tenor and pick a random link
         if ctx.author == bot.user:
             return
         async with ctx.typing():
             if random.random() <= 0.1:
-                await ctx.send('https://cdn.discordapp.com/attachments/906230633540485164/1316329779624153118/caption.gif?ex=675aa723&is=675955a3&hm=1826753181d4dc7bb5bd79442b2a74b824fee9b7c03ea1242a5386612e3e74aa&')
+                await ctx.send('https://tenor.com/view/bouncing-marisa-fumo-marisa-kirisame-touhou-fumo-gif-16962360816851147092')
             else:
                 await ctx.send(random.choice(self.MARISAD_GIFS))
-
 
 class DevCog(cmds.Cog, name='Dev'):
     def __init__(self, bot):
         self.bot = bot
 
-    @cmds.command("kys", help="I will KYS :)")
+    async def cog_check(self, ctx: cmds.Context): # type: ignore
+        MOMOYON_USER_ID: int = int(os.environ["MOMOYON_USER_ID"])
+        return ctx.author.id == MOMOYON_USER_ID
+
+    async def cog_command_error(self, ctx: cmds.Context, error: Exception) -> None:
+        assert type(ctx.command) == cmds.Command
+        embed = ds.Embed(title="Error")
+        if isinstance(error, cmds.CommandInvokeError):
+            error = error.original
+        embed.description = f"Error: {error}"
+
+        embed.description += f"\nUsage: {ctx.command.usage}"
+        momoyon: ds.User = await bot.fetch_user(int(os.environ["MOMOYON_USER_ID"]))
+        await ctx.send(f"Only {momoyon.mention} can use dev commands")
+        my_logging.bot_error(f"{self.qualified_name}Cog :: {type(error)}")
+        await ctx.send(embed=embed)
+
+    @cmds.command("kys", help="I will KYS :)", usage="kys")
     async def kys(self, ctx):
         KYS_REPONSES = [
                 "Wai-",
@@ -337,9 +330,6 @@ class DevCog(cmds.Cog, name='Dev'):
                 "AARGh-",
                 ":skull:",
         ]
-        if ctx.author.name != '.momoyon':
-            await ctx.send("What about *YOU* kys?")
-            return
         await ctx.send(random.choice(KYS_REPONSES))
         await ctx.bot.close()
 
@@ -347,7 +337,7 @@ class DevCog(cmds.Cog, name='Dev'):
 
 @bot.event
 async def on_ready():
-    log_info(f'{bot.user} logged in!')
+    my_logging.bot_info(f'{bot.user} logged in!')
 
 @bot.event
 async def on_message(msg):
@@ -355,7 +345,7 @@ async def on_message(msg):
         return
 
     # TODO: Handle msg.guild == None case
-    log_info(f"[{msg.created_at}][{msg.guild.name}::{msg.channel.name}] {msg.author}: '{msg.content}'")
+    my_logging.bot_info(f"[{msg.created_at}][{msg.guild.name}::{msg.channel.name}] {msg.author}: '{msg.content}'")
     await bot.process_commands(msg)
 
 async def add_cogs():
