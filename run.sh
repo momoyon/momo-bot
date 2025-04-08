d268593a2803e452c35b3a92acbb5390ad06b98d
ahmedsamyh <ahmedsamyh10@gmail.com>
2025-04-08 23:18:46 +0500

    [run.sh] Setup venv and install required modules.

--- run.sh
+++ run.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
-. ./.venv/bin/activate
+VENV_PATH=./.venv
+
+if [ ! -d "$VENV_PATH" ]; then
+    echo WARNING: Python venv dir not found...
+    echo INFO: Creating Python venv in $VENV_PATH...
+    python -m venv $VENV_PATH
+fi
+
+. $VENV_PATH/bin/activate
+
+pip install discord.py pynacl yt_dlp python-dotenv coloredlogs aiofile
 
 python ./bot.py $*
 
