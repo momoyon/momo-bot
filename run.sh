76c13bbe775756e9d2e14b7374d42f6a3d8501a7
ahmedsamyh <ahmedsamyh10@gmail.com>
2025-04-08 23:21:02 +0500

    [run.sh] Check for python in PATH.

--- run.sh
+++ run.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
 VENV_PATH=./.venv
+PYTHON=python
+
+if [ ! $(command -v $PYTHON) ]; then
+    PYTHON=python3
+    if [ ! $(command -v $PYTHON) ]; then
+        echo "ERROR: `python` nor `python3` is in PATH!"
+        exit 1
+    fi
+fi
 
 if [ ! -d "$VENV_PATH" ]; then
     echo WARNING: Python venv dir not found...
     echo INFO: Creating Python venv in $VENV_PATH...
-    python -m venv $VENV_PATH
+    $PYTHON -m venv $VENV_PATH
 fi
 
 . $VENV_PATH/bin/activate
